2976ef4a702876c4f5eb877185e40d3845eb8cc0
Amoga devi <amogadevi.s10@gmail.com>
2025-09-08 05:28:59 +0000

    still make it different user should attract (_for element svg_)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { LoginForm } from "@/components/auth/login-form";
 
 const LoginIllustration = () => (
     <svg
-      className="h-48 w-48 text-primary"
+      className="h-64 w-64 text-primary"
       viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
@@ -11,9 +11,9 @@ const LoginIllustration = () => (
       strokeLinecap="round"
       strokeLinejoin="round"
     >
-      <path d="M15 3h4a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2h-4" />
-      <polyline points="10 17 15 12 10 7" />
-      <line x1="15" y1="12" x2="3" y2="12" />
+      <path d="M8 11V7a4 4 0 1 1 8 0v4" />
+      <path d="M12 21a9 9 0 1 1 0-18 9 9 0 0 1 0 18Z" />
+      <path d="M12 16a2 2 0 0 1-2-2v-2a2 2 0 1 1 4 0v2a2 2 0 0 1-2 2Z" />
     </svg>
   );
 
