38c7a9e8d3a920ece38f8e1680aeb304aa01bc89
Amoga devi <amogadevi.s10@gmail.com>
2025-09-08 08:15:04 +0000

    background light blue still resduce the image (_for element img_)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,12 +6,12 @@ export default function Home() {
   return (
     <main className="grid min-h-screen w-full items-center justify-center md:grid-cols-2">
       <div className="relative hidden h-full items-center justify-center bg-muted/20 p-10 md:flex">
-        <div className="relative h-[450px] w-[300px]">
+        <div className="relative h-[400px] w-[250px]">
           <Image
             src="/logg_in.png"
             alt="Professional working on a laptop"
-            width={300}
-            height={450}
+            width={250}
+            height={400}
             className="rounded-lg object-cover"
             data-ai-hint="professional laptop"
           />
